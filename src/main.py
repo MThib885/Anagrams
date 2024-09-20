77699471623e3205ec9fefb63c2d9a624c55de4d
Thibaudeau <THIBAUDEAUM@science.regn.net>
2024-09-20 17:07:05 +0100

    Added dictionary

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from itertools import permutations
 
+with open("src/words.txt","r") as stream:
+    all_words = stream.read().split("\n")
+
 def anagrams(word):
     return ["".join(i) for i in permutations(list(word),len(word))]    
-
-print(anagrams("abc"))
