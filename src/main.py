7e3d1a77559655e009e2d38fc0e8897d90653657
Thibaudeau <THIBAUDEAUM@science.regn.net>
2024-09-24 16:29:05 +0100

    Add command line inputs

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
+import sys
 from itertools import permutations
 
-with open("src/words.txt","r") as stream:
-    all_words = stream.read().split("\n")
 
 def anagrams(word):
-    return ["".join(i) for i in permutations(list(word),len(word))]    
+    return ["".join(i) for i in permutations(list(word),len(word)) if "".join(i) in all_words]    
+
+
+if __name__ == "__main__":
+    with open("src/words.txt","r") as stream:
+        all_words = stream.read().split("\n")
+
+    if len(sys.argv) <= 1:
+        print("Give me something to anagram!")
+    else:
+        print(anagrams(sys.argv[1]))
