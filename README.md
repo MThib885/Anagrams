027e4f24b7d5bb5a57dcb7b7b3833b9ce5243cf0
Thibaudeau <THIBAUDEAUM@science.regn.net>
2024-09-24 16:45:50 +0100

    Added black

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# anagrams
+# Anagrams
+
+Find all valid English anagrams of a set of letters.
+
+## Installing
+
+No dependencies :)
+
+## Tooling
+
+### Formatters
+
+To run the formatter
+
+```bash
+python -m black src
+```
--- src/main.py
+++ src/main.py
@@ -3,11 +3,15 @@ from itertools import permutations
 
 
 def anagrams(word):
-    return ["".join(i) for i in permutations(list(word),len(word)) if "".join(i) in all_words]    
+    return [
+        "".join(i)
+        for i in permutations(list(word), len(word))
+        if "".join(i) in all_words
+    ]
 
 
 if __name__ == "__main__":
-    with open("src/words.txt","r") as stream:
+    with open("src/words.txt", "r") as stream:
         all_words = stream.read().split("\n")
 
     if len(sys.argv) <= 1:
